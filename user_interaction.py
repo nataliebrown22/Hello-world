a4b5bd2c0ded9a7d01305f1833a9fb739e6fd2f8
Natalie Brown <159576483+nataliebrown22@users.noreply.github.com>
2024-02-09 13:39:34 -0600

    Add files via upload

---
+++ user_interaction.py
@@ -0,0 +1,16 @@
+#user interactions with if statements
+print("Welcome user")
+name = input("What would you like to be called, enter a name: ")
+print("Hello", name),
+mood = input("How are you doing today, good or bad? ")
+
+if mood == "good":
+    print("Glad to hear you are doing well!")
+elif mood == "bad":
+    print("Im sorry to hear that.")
+else:
+    print("error, enter good or bad")
+
+
+
+
